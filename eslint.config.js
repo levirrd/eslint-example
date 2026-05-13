43a0015e282cf684894f38d5d2c59f26d8082469
leventedemsa <demsa.levente@ganziskola.hu>
2026-05-13 20:23:26 +0200

    fix: configure ESLint TypeScript parsing

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,13 @@
 import plugin from "./eslint-local/index.js";
 import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
     {
-        files: ["**/*.{ts,mts}"],
+files: ["**/*.{ts,mts}"],
+languageOptions: {
+    parser: tseslint.parser,
+},
         plugins: {
             plugin,
         },
